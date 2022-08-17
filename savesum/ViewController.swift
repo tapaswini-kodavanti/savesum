2ec83f3688301ec95650c1ed8fb98f5ef47fcd69
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2022-08-17 09:52:11 -0500

    Added UI for Goal Input Page

--- savesum/ViewController.swift
+++ savesum/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
     var s = "Checking to see if the push worked"
 
 }
